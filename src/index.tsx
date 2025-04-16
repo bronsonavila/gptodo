5ad0fdcda3d3b1c808fac712ba09d079129c1eed
Bronson Avila <bronson.avila@gmail.com>
2025-04-15 19:53:11 -1000

    Fix entry

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ErrorBoundary } from './components/ErrorBoundary'
 
-const rootEl = document.getElementById('root')
-
-if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl)
-
-  root.render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ErrorBoundary>
       <App />
-    </React.StrictMode>
-  )
-}
+    </ErrorBoundary>
+  </React.StrictMode>
+)
