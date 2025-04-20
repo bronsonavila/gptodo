2006d8e967e782b7ea6c7c0c1da018662ffda1fd
Bronson Avila <bronson.avila@gmail.com>
2025-04-20 00:59:16 -1000

    Add Google Tag

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,11 +4,32 @@ import { pluginReact } from '@rsbuild/plugin-react'
 export default defineConfig({
   html: {
     favicon: './public/favicon.svg',
+
     meta: {
       author: 'Bronson Avila',
       description: 'GPTodo uses AI to convert photos of handwritten or typed to-do lists into interactive checklists.',
       keywords: 'ai, checklist, gemini, image recognition, list, todo, vision'
     },
+
+    tags: [
+      {
+        tag: 'script',
+        attrs: {
+          async: true,
+          src: `https://www.googletagmanager.com/gtag/js?id=${process.env.PUBLIC_GOOGLE_ANALYTICS_ID}`
+        }
+      },
+      {
+        tag: 'script',
+        children: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${process.env.PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `
+      }
+    ],
+
     title: 'GPTodo'
   },
 
